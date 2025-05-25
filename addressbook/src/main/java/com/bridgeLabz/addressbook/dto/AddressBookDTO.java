21ef0a1508436cfb86afad64020d7f3b193038de
Madhumitha A <137876422+madhu1703@users.noreply.github.com>
2025-05-25 16:06:29 +0530

    UC1Lombok added

--- addressbook/src/main/java/com/bridgeLabz/addressbook/dto/AddressBookDTO.java
+++ addressbook/src/main/java/com/bridgeLabz/addressbook/dto/AddressBookDTO.java
@@ -1,24 +1,14 @@
 package com.bridgeLabz.addressbook.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressBookDTO {
     private String name;
     private String address;
     private String email;
-
-    public AddressBookDTO() {}
-
-    public AddressBookDTO(String name, String address, String email) {
-        this.name = name;
-        this.address = address;
-        this.email = email;
-    }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
 }
