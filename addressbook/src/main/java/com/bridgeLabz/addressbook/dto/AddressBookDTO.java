5ad90da2f44d5ebaae32695731abd360d28bb6a3
Madhumitha A <137876422+madhu1703@users.noreply.github.com>
2025-05-25 19:34:57 +0530

    added UC1Validation

--- addressbook/src/main/java/com/bridgeLabz/addressbook/dto/AddressBookDTO.java
+++ addressbook/src/main/java/com/bridgeLabz/addressbook/dto/AddressBookDTO.java
@@ -1,5 +1,7 @@
 package com.bridgeLabz.addressbook.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddressBookDTO {
+
+    @NotBlank(message = "Name is required")
+    @Pattern(regexp = "^[A-Za-z\\s]{2,30}$", message = "Name must be 2 to 30 characters long and contain only letters and spaces")
     private String name;
+
     private String address;
     private String email;
 }
